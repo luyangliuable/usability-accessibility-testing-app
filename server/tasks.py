283945e97249ecc78fff6721f4c922fd05d96674
Luyang Liu <luyangl16@gmail.com>
2022-05-01 17:21:13 +1000

    Prepared basic docker, mongodb, redis, flask celery and files.

---
+++ server/tasks.py
@@ -0,0 +1,17 @@
+from project.machine_learning import app as machine_learning
+from flask import jsonify, send_file
+from celery import Celery
+import time
+import os
+
+celery = Celery(__name__)
+celery.conf.broker_url = os.environ['REDIS_URL']
+celery.conf.result_backend = os.environ['REDIS_URL']
+
+
+@celery.task(name="create_task")
+def create_task(info):
+    ###############################################################################
+    #                           Create celery web tasks                           #
+    ###############################################################################
+    pass

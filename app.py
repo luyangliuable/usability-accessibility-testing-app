df4e839ab9f5ec735a1bbf68eb85a250432600b2
abaj0005 <abaj0005@student.monash.edu>
2022-05-02 17:48:15 +1000

    Added different endpoints for registering and logging in user
    Also added support for storing new users in mongodb database, with an added User model class.

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 from flask import Flask
+from flask import request
+import os
+import pymongo
+
+from server.models.User import *
 
 app = Flask(__name__)
+try:
+    mongo = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = mongo.users_db
+    mongo.server_info()  # Triger exception if connection fails to the database
+except Exception as ex:
+    print('failed to connect', ex)
+
+
 @app.route('/')
 def home():
     return 'Flask with docker!'
+
+
+@app.route('/signUp', methods=['POST'])
+def signUpUser():
+    return UserModel().signUpUser()
+
+
+@app.route('/login', methods=['POST'])
+def loginUser():
+    return UserModel().loginUser()
 
 
 if __name__ == "__main__":

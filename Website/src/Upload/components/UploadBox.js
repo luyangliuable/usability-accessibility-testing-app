c233caf5b63019e2d76c140d452fd4a07a8ec86a
Mark-Diedericks <mark.diedericks830@gmail.com>
2022-05-17 18:48:42 +1000

    Upload APK/MP4 file dropbox - Added file drop box to upload page - Added open file dialog to upload page - Added file upload post requests to upload page

--- Website/src/Upload/components/UploadBox.js
+++ Website/src/Upload/components/UploadBox.js
@@ -1,11 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component, useCallback, useRef, useState } from 'react'
+import { useDropzone } from 'react-dropzone'
+import superagent from 'superagent'
 
 import "./ResultBox.css"
 
-export default class UploadBox extends Component {
-  render() {
-    return (
-      <div className="result-box-root">
+export default function UploadBox() {
+  var [buttonState, setButtonState] = useState(false);
+  var [selectedFile, setSelectedFile] = useState(null);
+
+  var canAccept = (file) => {
+    /* TODO check file types */
+    return null
+  };
+
+  const onDropAccepted = useCallback(acceptedFiles => {
+    setButtonState(true);
+    setSelectedFile(acceptedFiles[0])
+
+    /* todo backend upload url */
+    var req = superagent.post('http://localhost/upload/');
+    acceptedFiles.forEach(file => {
+      req.attach(file.name, file);
+    });
+
+    req.end((resp) => console.log(resp));
+  });
+
+  var { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDropAccepted: onDropAccepted,
+    maxFiles: 1,
+    disabled: buttonState,
+    validator: canAccept
+  });
+
+
+  return (
+    <div className="result-box-root">
+        <div{ ...getRootProps() } disabled={buttonState}>
+          <input { ...getInputProps()} disabled={buttonState}/>
+
           <div className="result-box-full-width">
               <img className="result-box-icon" src={require("./content/dummy_200x256.png")}  />
               <img className="result-box-icon" src={require("./content/dummy_200x256.png")}  />
@@ -14,7 +47,7 @@ export default class UploadBox extends Component {
           <div className="result-vspacing-10"> </div>
 
           <div className="result-box-full-width">
-              <p className="result-box-text-30">Drop APK or MP4 here</p>
+              <p className="result-box-text-30">{selectedFile ? selectedFile.name : 'Drop APK or MP4 here'}</p>
           </div>
 
           <div className="result-box-full-width"> 
@@ -22,15 +55,17 @@ export default class UploadBox extends Component {
               {/* TODO actual progess bar */}
               <div className="result-box-line result-box-left" />
               <div className="result-box-line result-box-right" />
+
               <p className="result-box-text-20 result-text-center">or</p>
             </div>
           </div>
 
           <div className="result-box-full-width"> 
               {/* TODO functional button */}
-            <button className="result-box-view-button result-button-enabled">Upload File</button>
+            <button className={buttonState ? "result-box-view-button result-button-disabled" : "result-box-view-button result-button-enabled"}
+                disabled={buttonState}>{buttonState ? 'Uploading...' : 'Upload File'}</button>
           </div>
-      </div>
-    )
-  }
+        </div>
+    </div>
+  )
 }

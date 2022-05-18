3a599565f9e6ce4d553cbfc0a227c378dbe50056
Luyang Liu <luyangl16@gmail.com>
2022-05-18 20:27:43 +1000

    Modified save apk_file to redis to use uid instead of static string

--- server/upload/app.py
+++ server/upload/app.py
@@ -1,10 +1,16 @@
 from flask import Blueprint, request
 from redis import Redis
+import uuid
+import random
 import pickle
 import os
 
 upload_blueprint = Blueprint("upload", __name__)
-r = Redis.from_url(os.environ.get('REDIS_URL'))
+# r = Redis.from_url(os.environ.get('REDIS_URL'))
+
+def unique_id_generator():
+    res = "apk_file_" + str( uuid.uuid4() )
+    return res
 
 @upload_blueprint.route('/upload', methods=["GET", "POST"])
 def upload():
@@ -16,3 +22,9 @@ def upload():
         r.set("apk_file", zlib.compress(pickle.dumps(df)))
 
     return "Upload Is Online"
+
+if __name__ == "__main__":
+    print(unique_id_generator())
+    print(unique_id_generator())
+    print(unique_id_generator())
+    print(unique_id_generator())

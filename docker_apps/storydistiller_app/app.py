220b7d0e9023d49bc6d71214ce7121bdd21ce182
Trevin Wadu <trevinwadu@gmail.com>
2022-05-25 17:42:30 +1000

    storydistiller api fully functional

--- docker_apps/storydistiller_app/app.py
+++ docker_apps/storydistiller_app/app.py
@@ -2,45 +2,83 @@
 import boto3
 import os
 import subprocess
+from flask import Flask, request, jsonify
 
 
+app = Flask(__name__)
+
+# aws info for development environment
 AWS_REGION = 'us-west-2'
 AWS_PROFILE = 'localstack'
 ENDPOINT_URL = os.environ.get('S3_URL')
 
+# aws s3 client
 boto3.setup_default_session(profile_name=AWS_PROFILE)
 s3_client = boto3.client("s3", region_name=AWS_REGION,
                          endpoint_url=ENDPOINT_URL)
 
+# home route
+@app.route('/')
+def home():
+    return "Story distiller app is live."
 
-def service_execute(uuid):
+# route for starting new job
+@app.route("/new_job", methods=["POST"])
+def send_uid_and_signal_run():
+    if request.method == "POST":
+        try:
+            print('NEW JOB FOR UUID: ' + request.get_json()["uid"])
+            uid = request.get_json()["uid"]
+        except:
+            return 'Invalid uuid data'
+
+        _service_execute(uid)
+
+        return jsonify( {"result": "SUCCESS"} ), 200
+
+
+    return "No HTTP POST method received"
+
+# executing new job
+def _service_execute(uuid):
+    print('Beginning new job for %s' % uuid)
+
     apk_name = _get_apk_name(uuid)
+    print('APK name for %s is %s' % (uuid, apk_name))
 
     # backup original source code
     subprocess.run(["cp", "-r", "/home/StoryDistiller-main", "/home/tmp/StoryDistiller-main"])
-
+    
+    print('Downloading APK')
     _get_data(uuid, apk_name)
+    print('Successfully Downloaded')
 
+    print('Running StoryDistiller')
     _process_result()
+    print('Successfully ran')
 
+    print('Uploading results')
     _upload_result(uuid, apk_name)
+    print('Successfully uploaded')
 
     # restore original source code
     subprocess.run(["rm", "-r", "/home/StoryDistiller-main"])
     subprocess.run(["cp", "-r", "/home/tmp/StoryDistiller-main", "/home/StoryDistiller-main"])
     subprocess.run(["rm", "-r", "/home/tmp/StoryDistiller-main"])
 
+    print('Job for %s complete' % uuid)
+
 # get name of file stored where key==uuid in s3
 def _get_apk_name(uuid):
-    response = s3_client.list_objects_v2(Bucket='apk-bucket')
+    response = s3_client.list_objects_v2(Bucket='apk-bucket', Prefix=uuid)
     contents = response['Contents']
-    apk_name = contents[0]['Key']
+    apk_name = contents[0]['Key'].replace(uuid+'/', '').replace('.apk', '')
     return apk_name
 
 # get the required inputs from s3
 def _get_data(uuid, apk_name):
     filepath = '/home/StoryDistiller-main/main-folder/apks/%s.apk' % apk_name
-    s3_client.download_file('apk-bucket', uuid, filepath)
+    s3_client.download_file('apk-bucket', '%s/%s.apk' % (uuid, apk_name), filepath)
 
 # run the algorithm
 def _process_result():
@@ -76,5 +114,7 @@ def _upload_result(uuid, apk_name):
 
 
 if __name__=='__main__':
-    # test run
-    service_execute('a2dp.Vol_133')
+    app.run(debug=True, host="0.0.0.0", port=3002)
+    
+    # test service
+    #service_execute('a2dp.Vol_133')

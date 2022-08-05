e290d7a8690a51fbbdc6e84a66e1a59a6a4af328
smao0001 <smao0001@student.monash.edu>
2022-08-05 13:40:08 +1000

    Change the style of toolbar

--- Website/src/ToolBar/ToolBar.js
+++ Website/src/ToolBar/ToolBar.js
@@ -1,42 +1,29 @@
-import React from "react";
-import { Row, Container, Col } from "react-bootstrap";
-import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-import "./ToolBar.css"
-
-export default function ToolBar() {
+function BasicExample() {
   return (
-      <Container className="nav-container" fluid='true'>
-        <Row>
-          <Col xxl={1} className="company-logo" >
-            <Link to={"./"}>
-              <img alt='logo' src={require('./favicon.ico')} />
-            </Link>
-          </Col>
-          <Col xxl={9} className="middle-bar">
-            <Col className="nav-center">
-              <Link to={"./about"}>
-                <h3>ABOUT</h3>
-              </Link>
-            </Col>
-              <Col className="nav-center">
-                <Link to={"./upload"}>
-                  <h3>UPLOAD FILE</h3>  
-                </Link>  
-              </Col>
-              <Col className="nav-center">
-                <Link to={"./results"}>
-                  <h3>RESULTS</h3>
-                </Link>
-              </Col> 
-          </Col>
-          <Col xxl={2} className="nav-right">
-            <Link to={"./login"}>
-              <h3>LOGIN</h3>
-            </Link>
-          </Col>
-
-        </Row>
+    <Navbar bg="black" variant="dark">
+      <Container>
+        <Navbar.Brand href="./">
+          <img alt='logo' src={require('./BXER.png')} height="100" width="100"/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto" >
+            <Nav.Link href="./about" style={{color:"rgb(219, 218, 218)"}} >About</Nav.Link>
+            <Nav.Link href="./upload" style={{color:"rgb(219, 218, 218)"}} >Upload</Nav.Link>
+            <Nav.Link href="./results" style={{color:"rgb(219, 218, 218)"}}>Results</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="./login" style={{color:"rgb(219, 218, 218)"}}>Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
-  )
+    </Navbar>
+  );
 }
+
+export default BasicExample;

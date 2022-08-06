6abb3906a3791b20a5fbb8907d602eeb2ac10859
Emily <elam0011@student.monash.edu>
2022-08-06 12:40:53 +1000

    Update pipeline to create class

--- tappable/pipeline.py
+++ tappable/pipeline.py
@@ -5,60 +5,89 @@ from skimage import io, transform
 from model import ResNet, Block
 import os
 
-#Applies binary mask of button onto image matrix
-def applyMask(img, object_bounds):
-    width = img.shape[0]
-    height = img.shape[1]
+class ModelPipeline:
 
-    binary_mask = np.zeros(shape=(width, height))
-    x_ratio_min = object_bounds[0]/width
-    x_ratio_max = object_bounds[2]/width
-    y_ratio_min = object_bounds[1]/height
-    y_ratio_max = object_bounds[3]/height
-        
-    for x in range(width):
-        for y in range(height):
-            if x_ratio_min <= x/width < x_ratio_max and y_ratio_min <= y/height < y_ratio_max:
-                binary_mask[x,y] = 1 
-    concat = np.dstack((img, binary_mask)) 
-    return concat
+    def __init__(self, img_path, bounds):
+        self.img_path = img_path
+        self.bounds = self.updateArray(bounds)
+        self.img = self.getImage()
 
-#Converts image to tensor
-def toTensor(img):
-    transpose_img = img.transpose((2, 0, 1))
-    return torch.from_numpy(transpose_img)
+    #Gets stored image [TODO: update to get image from s3 bucket]
+    def getImage(self):
+        self.validateImage()
+        return io.imread(os.getcwd() + self.img_path)
 
-#Apply Transformations
-def image_transformations(img, bounds):
-    image = transform.resize(img, (960, 540))
-    mask_img = applyMask(image, bounds)
-    tensor_img = toTensor(mask_img)
-    tensor = tensor_img.unsqueeze(0)
-    input = tensor.type(torch.FloatTensor)
-    return input
+    def updateArray(self, bounds):
+        for i in range(len(bounds)):
+            bounds[i] = int(bounds[i])
+        return bounds
+            
+    #Applies binary mask of button onto image matrix
+    def applyMask(self, resizeImg):
+        width = resizeImg.shape[0]
+        height = resizeImg.shape[1]
 
-#Get image path
-config = ConfigParser()
-config.read('config.ini')
-img_path = config.get('main', 'image')
-img = io.imread(os.getcwd() + img_path) #TODO: Change to accept uploaded image
+        binary_mask = np.zeros(shape=(width, height))
+        x_ratio_min = self.bounds[0]/width
+        x_ratio_max = self.bounds[2]/width
+        y_ratio_min = self.bounds[1]/height
+        y_ratio_max = self.bounds[3]/height
+            
+        for x in range(width):
+            for y in range(height):
+                if x_ratio_min <= x/width < x_ratio_max and y_ratio_min <= y/height < y_ratio_max:
+                    binary_mask[x,y] = 1 
+        concat = np.dstack((resizeImg, binary_mask)) 
+        return concat
 
-#Get object bounds
-bounds = config.get('main', 'bounds')
-bounds = [0,2217, 0, 2392] #clickable 
+    #Converts image to tensor
+    def toTensor(self, maskImg):
+        transpose_img = maskImg.transpose((2, 0, 1))
+        return torch.from_numpy(transpose_img)
 
-#Apply transformations to image
-input = image_transformations(img, bounds)
+    #Apply Transformations
+    def image_transformations(self):
+        img_resize = transform.resize(self.img, (960, 540))
+        mask_img = self.applyMask(img_resize)
+        tensor_img = self.toTensor(mask_img)
+        tensor = tensor_img.unsqueeze(0)
+        float_tensor = tensor.type(torch.FloatTensor)
+        return float_tensor
 
-#Create model from saved state
-model = ResNet(18, Block, 4, 1000)
-model.load_state_dict(torch.load('resnet.pt', map_location=torch.device('cpu')))
-model.eval()
+    #Image validation
+    def validateImage(self):
+        if self.img_path[-3:] != 'jpg':
+            raise Exception("File must be of type jpg")
 
-#Prediction
-labels = ['0' ,'1', '2', '3', '4', '5'] 
-with torch.no_grad():
-    predictions = model(input)
-    _, index = torch.max(predictions, 1)
-    percentage = torch.nn.functional.softmax(predictions, dim=1)[0] * 100
-    print(str(round(percentage[index[0]].item(),2)) + "%; rated " + labels[index[0]] + "/5 tappable")
+    def modelPipeline(self):
+        #Apply Image Transformations
+        input = self.image_transformations()
+
+        #Create model from saved state
+        model = ResNet(18, Block, 4, 1000)
+        model.load_state_dict(torch.load('resnet.pt', map_location=torch.device('cpu')))
+        model.eval()
+
+        #Prediction
+        labels = ['0' ,'1', '2', '3', '4', '5'] 
+        with torch.no_grad():
+            predictions = model(input)
+            _, index = torch.max(predictions, 1)
+            percentage = torch.nn.functional.softmax(predictions, dim=1)[0] * 100
+            print(str(round(percentage[index[0]].item(),2)) + "%; rated " + labels[index[0]] + "/5 tappable")
+
+
+if __name__ == '__main__':
+    #Read config file
+    config = ConfigParser()
+    config.read('config.ini')
+
+    #Get image path
+    img_path = config.get('main', 'image')
+
+    #Get object bounds
+    bounds = config.get('main', 'bounds')
+    bounds_array = bounds.strip('[]').split(',')
+
+    prediction = ModelPipeline(img_path, bounds_array)
+    prediction.modelPipeline()

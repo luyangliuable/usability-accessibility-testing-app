6168fb9b394df1d7c87502b87f9aefe51d29bfce
Mark-Diedericks <mark.diedericks830@gmail.com>
2022-08-09 12:45:15 +1000

    - Fixed uploading of files - Storydistiller app starts - Xbot app starts - Storydistiller fails results ZIP - XBot fails algorithm starting

--- server/tasks.py
+++ server/tasks.py
@@ -21,16 +21,16 @@ def create_task(info):
     # TODO signal all algorithms to start.
 
     # Story distiller api url to be obtained from the enrionemtn ###############
-    story_distiller_api = os.environ.get("STORYDISTILLER")
-    story_distiller_api = "http://storydistiller:3002/new_job"
+    # story_distiller_api = os.environ.get("STORYDISTILLER")
+    story_distiller_api = "http://host.docker.internal:3002/new_job"
 
     # xbot api url to be obtained from the enrionemtn ##########################
-    xbot_api = os.environ.get("XBOT")
-    xbot_api = "http://xbot:3002/new_job"
+    # xbot_api = os.environ.get("XBOT")
+    xbot_api = "http://host.docker.internal:3003/new_job"
 
     # xbot api url to be obtained from the enrionemtn ##########################
-    owleye_api = os.environ.get("XBOT")
-    owleye_api = "http://xbot:3002/new_job"
+    # owleye_api = os.environ.get("OWLEYE")
+    owleye_api = "http://host.docker.internal:3004/new_job"
 
     print(info)
     uuid = info['uuid']
@@ -55,7 +55,11 @@ def create_task(info):
     ###############################################################################
     try:
         algorithm = "storydistiller"
-        requests.post(story_distiller_api, data={ "uid": uuid })
+        result = requests.post(story_distiller_api, json={ "uid": uuid })
+
+        print(result.status_code)
+        print(result.text)
+        print(result.content)
     except Exception as ex:
         print('failed to complete tasks', algorithm, "with url", story_distiller_api, "because", ex)
         errors.append(ex)
@@ -69,27 +73,32 @@ def create_task(info):
     ###############################################################################
     try:
         algorithm = "xbot"
-        requests.post(xbot_api, data={ "uid": uuid })
+        result = requests.post(xbot_api, json={ "uid": uuid })
+
+        print(result.status_code)
+        print(result.text)
+        print(result.content)
     except Exception as ex:
-        print('failed to complete tasks', algorithm, "with url", owleye_api, "because", ex)
+        print('failed to complete tasks', algorithm, "with url", xbot_api, "because", ex)
         errors.append(ex)
     else:
         print("Successfully connected completed tasks", algorithm)
-        current_task.update_state('PROGRESS', meta={"task_id": "", "task_status": ['distiller'], "task_result": "sadasd"})
+        current_task.update_state('PROGRESS', meta={"task_id": "", "task_status": ['xbot'], "task_result": "sadasd"})
 
 
     ###############################################################################
     #                                   owleye                                    #
     ###############################################################################
-    try:
-        algorithm = "owleye"
-        requests.post(owleye_api, data={ "uid": uuid })
-    except Exception as ex:
-        print('failed to complete tasks', algorithm, "with url", owleye_api, "because", ex)
-        errors.append(ex)
-    else:
-        print("Successfully connected completed tasks", algorithm)
-        current_task.update_state('PROGRESS', meta={"task_id": "", "task_status": ['distiller'], "task_result": "sadasd"})
+    # try:
+    #     algorithm = "owleye"
+    #     result = requests.post(owleye_api, json={ "uid": uuid })
+    #     print(result)
+    # except Exception as ex:
+    #     print('failed to complete tasks', algorithm, "with url", owleye_api, "because", ex)
+    #     errors.append(ex)
+    # else:
+    #     print("Successfully connected completed tasks", algorithm)
+    #     current_task.update_state('PROGRESS', meta={"task_id": "", "task_status": ['owleye'], "task_result": "sadasd"})
 
     # time.sleep(5)
     print("task completed")
--- server/upload/app.py
+++ server/upload/app.py
@@ -88,6 +88,10 @@ def upload():
 
         print(temp_file_name, "is created")
 
+
+        enforce_bucket_existance(["apk-bucket", "storydistiller-bucket", "xbot-bucket"])
+
+
         ###############################################################################
         #                   Upload the temporary file to git bucket                   #
         ###############################################################################
@@ -177,6 +181,17 @@ def after_request(response):
     # response.headers.add('access-control-allow-headers', 'authorization')
     # response.headers.add('Access-Control-Allow-Methods', 'POST')
     return response
+
+
+
+
+def enforce_bucket_existance(buckets):
+    for bucket in buckets:
+        try:
+            s3_client.create_bucket(Bucket=bucket, CreateBucketConfiguration={'LocationConstraint': 'us-west-2'})
+        except:
+            print("Bucket already exists %s".format( bucket ))
+
 
 
 

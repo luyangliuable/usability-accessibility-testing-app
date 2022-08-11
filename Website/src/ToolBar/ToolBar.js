323e42f3ecdddf600af4b5efd91ffbae2e06a0b4
eche0034 <you@example.com>
2022-08-12 01:46:09 +1000

    highlight current link, toolbar changes

--- Website/src/ToolBar/ToolBar.js
+++ Website/src/ToolBar/ToolBar.js
@@ -1,28 +1,42 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import { Row, Container, Col } from "react-bootstrap";
+import { Link } from 'react-router-dom';
 
-function BasicExample() {
+import "./ToolBar.css"
+
+export default function ToolBar() {
   return (
-    <Navbar bg="black" variant="dark">
-      <Container>
-        <Navbar.Brand href="./">
-          <img alt='logo' src={require('./BXER.png')} height="100" width="100"/>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto" >
-            <Nav.Link href="/about" style={{ color: "rgb(219, 218, 218)" }} >About</Nav.Link>
-            <Nav.Link href="/upload" style={{ color: "rgb(219, 218, 218)" }} >Upload</Nav.Link>
-            <Nav.Link href="/results" style={{ color: "rgb(219, 218, 218)" }}>Results</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href="/login" style={{ color: "rgb(219, 218, 218)" }}>Login</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
+      <Container className="nav-container" fluid='true'>
+        <Row>
+          <Col xxl={2}className="company-logo" >
+            <Link to={"./"}>
+              <img alt='logo' src={require('./BXER.png') } width = "75%" height = "75%"/>
+            </Link>
+          </Col>
+            <Col xxl={9} className="middle-bar" id="nav">
+              <Col>
+                <Link to={"./about"}>
+                  <h1><a href="#">ABOUT</a></h1>
+                </Link>
+              </Col>
+              <Col >
+                <Link to={"./upload"}>
+                  <h1><a href="#">UPLOAD FILES</a></h1>  
+                </Link>  
+              </Col>
+              <Col>
+                <Link to={"./results"}>
+                  <h1><a href="#">RESULTS</a></h1>
+                </Link>
+              </Col> 
+              <Col>
+              </Col>
+              <Link to={"./login"}>
+                <h1>LOGIN</h1>
+              </Link>
+            </Col>
 
-export default BasicExample;
+        </Row>
+      </Container>
+  )
+}

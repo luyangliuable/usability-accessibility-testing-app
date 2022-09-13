c67398888124db011cdbd737f5c61c59e1a2a86d
Trevin Wadu <trevinwadu@gmail.com>
2022-09-13 22:27:21 +1000

    added emulator class

---
+++ algorithms/app/resources/emulator.py
@@ -0,0 +1,37 @@
+from typing_extensions import Self
+from resources import *
+from typing import List, Callable
+
+class Emulator:
+    """Class to manage emulators
+    When a task requires an emulator it can subscribe to a queue and provide a callback
+    When the emulator is available it will call the next subscriber with the emulator name 
+    """
+    
+    def __init__(self, name) -> None:
+        self.name = name
+        self.is_free = True
+        self.subscribers = []
+    
+    def subscribe(self, subscriber : Callable(str)) -> None:
+        """Subscriber for emulator"""
+        self.subscribers.append(subscriber) # add subscriber to queue
+        self._update_queue()
+        
+    
+    def free_emulator(self) -> None:
+        """Called when a subscriber is finished with the emulator"""
+        self.in_use = False
+        self._update_queue()
+        
+        
+    def _update_queue(self) -> None:
+        """Notifies next subscriber if emulator is free"""
+        # if there are more subscribers and emulator is free, notify the next
+        if len(self.subscribers) > 0 and self.is_free:  
+            self.subscribers.pop(0)(self.name)
+            self.is_free = False
+            
+        
+        
+        

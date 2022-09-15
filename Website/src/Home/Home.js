704a916628c694623fe89ca24d5ae81de7559cbe
lliu240 <lliu240@student.monash.edu>
2022-09-16 09:59:17 +1000

    Changes to landing page

--- Website/src/Home/Home.js
+++ Website/src/Home/Home.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
-import "./Home.css"
+import "./Home.css";
 
 export default class Home extends Component {
   render() {
     return (
       <Container fluid="true">
-        <Row className="hero">
+        <Row className="hero" style={{ background: "#111", }}>
+          <div classname="landing_button">Get Started</div>
             <Col className='hero-text'>
                 <h1>HELPING DEVELOPERS. <br/> HELPING USERS.</h1>
             </Col>
         </Row>
       </Container>
-    )
-  }
+    );
 }
+}
+
+
+const style = {
+    button: {
+    }
+
+};

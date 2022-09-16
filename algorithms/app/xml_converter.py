9112569f3a1e3543da261c6c29db756274846119
Emily <emilylam2205@gmail.com>
2022-09-16 11:34:59 +1000

    update to newer xml converter

--- algorithms/app/xml_converter.py
+++ algorithms/app/xml_converter.py
@@ -7,21 +7,16 @@ class xmlConverter():
         self.xml_path = xml_path
         self.json_path = json_path
 
-    def __update_list(self, json_children):
-        """
-        Loops through list of children and calls update on individual dictionary
-        """
+    def _update_list(self, json_children: list) -> list:
+        """Loops through list of children and calls update on individual dictionary"""
         out = []
         for child in json_children:
-            child_out = self.__update_dict(child)
+            child_out = self._update_dict(child)
             out.append(child_out)
         return out
 
-
-    def __update_dict(self, child):
-        """
-        Updates json file for dictionary item and children. Renames keys, change type of values and format null. 
-        """
+    def _update_dict(self, child: dict) -> dict:
+        """Updates json file for dictionary item and children. Renames keys, change type of values and format null"""
         if "node" not in child:
             child["child_count"] = 0
         children_copy = child.copy()
@@ -35,17 +30,25 @@ class xmlConverter():
                     else:
                         child["child_count"] = 1
                     if child["child_count"] > 1:
-                        child["children"] = self.__update_list(child["children"])
+                        child["children"] = self._update_list(child["children"])
                     else:
-                        child["children"] = self.__update_dict(child["children"])
+                        child["children"] = self._update_dict(child["children"])
                 elif key =="bounds":
                     re_text = r"\[(\d*?),(\d*?)\]\[(\d*?),(\d*?)\]"
                     bounds_out = re.findall(re_text, val)[0]
                     bounds_json = [[int(bounds_out[0]), int(bounds_out[1])], [int(bounds_out[2]), int(bounds_out[3])]]
                     child[key] = bounds_json
+                elif key == "focused":
+                    focused = bool(child.pop("focused"))
+                    child["visible"] = focused
                 elif key =="password":
                     pw = bool(child.pop("password"))
                     child["is_password"] = pw
+                elif key =="resource-id":
+                    ri = child.pop("resource-id")
+                    if ri =="":
+                        ri = None
+                    child["resource_id"] = ri
                 elif key =="content-desc":
                     cd = child.pop("content-desc")
                     if cd =="":
@@ -60,41 +63,39 @@ class xmlConverter():
                     child[key] = None
         return child
 
-    def __node_list(self, a, final, counter):
+    def _node_list(self, a: list, final: list, counter: int) -> list:
+        """Converts node heiarachy to node list"""
         if "children" in a:
             out = []
             if type(a["children"])==list:
                 for child in a["children"]:
-                    final, counter= self.__node_list(child.copy(), final.copy(), counter)
+                    final, counter= self._node_list(child.copy(), final.copy(), counter)
                     out.append(counter)
                     counter +=1
             else:
-                final, counter= self.__node_list(a["children"].copy(), final.copy(), counter)
+                final, counter= self._node_list(a["children"].copy(), final.copy(), counter)
                 out.append(counter)
                 counter +=1
             a["children"] = out.copy()
         final.append(a.copy())
         return final, counter
 
-
-    def convert_xml_to_json(self):
-        """
-        Converts storydistiller xml file to json format
-        """
-        with open(self.xml_path) as xml_file:
+    def _convert_xml_to_json(self, xml_path: str, json_path: str) -> None:
+        """Converts storydistiller xml file to json format"""
+        with open(xml_path) as xml_file:
             data_dict = xmltodict.parse(xml_file.read(),attr_prefix='')
             data_node = data_dict["hierarchy"]["node"]
 
-        json_file_updated= self.__update_dict(data_node)
+        json_file_updated= self._update_dict(data_node)
 
         final = []
         counter = 0
-        json_node_out, _= self.__node_list(json_file_updated, final, counter)
+        json_node_out, _= self._node_list(json_file_updated, final, counter)
 
         views_out = {"views": json_node_out}
 
         json_str = json.dumps(views_out, indent=4)
 
-        json_out = open(self.json_path, "w+")
+        json_out = open(json_path, "w+")
         json_out.write(json_str)
         json_out.close()

0c74b07e7c99b5dff11353da290990d2de73b329
dper21 <dper21@student.monash.edu>
2022-09-17 12:59:49 +1000

    Fixed Toolbar Spacing Issues`
    Buttons on page were too closely bunched together when resizing window. Issue is now fixed.

--- Website/src/ToolBar/ToolBar.js
+++ Website/src/ToolBar/ToolBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Row, Container, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
@@ -7,6 +7,7 @@ import "./ToolBar.css";
 
 export default function ToolBar() {
   const [user, updateUser] = useState(sessionStorage.getItem("User_UUID"));
+  
 
   console.log('user right now: ', user);
 
@@ -18,34 +19,35 @@ export default function ToolBar() {
   return (
     <Container className="nav-container" fluid="true">
       <Row>
-        <Col xs={1} className="centre-toolbar">
+        <Col style={{width: "max(50px, 5vw)"}} className="centre-toolbar">
           <Link to={"./"}>
             <img
               alt="logo"
               src={require("./content/BXER.png")}
-              style={{ height: "max(5vmin, 30px)" }}
+              style={{ height: "max(50px, 5vh)" }}
             />
           </Link>
         </Col>
-        <Col xs={9} className="centre-toolbar" id="nav">
+        <Col style={{width: "max(300px, 90vw)"}} className="centre-toolbar" id="nav">
           <Col>
             <NavLink to={"./about"}>
               <h1>ABOUT</h1>
             </NavLink>
           </Col>
+          <Col style={{width: "max(10px, 10vw)"}}></Col>
           <Col>
             <NavLink to={"./upload"}>
               <h1>UPLOAD</h1>
             </NavLink>
           </Col>
+          <Col style={{width: "max(10px, 10vw"}}></Col>
           <Col>
             <NavLink to={"./results"}>
               <h1>RESULTS</h1>
             </NavLink>
           </Col>
-          <Col xs={6}></Col>
         </Col>
-        <Col xs={1} className="centre-toolbar" id="nav">
+        <Col style={{width: "max(5px, 5vw)"}} className="centre-toolbar" id="nav">
           {user &&
             <div onClick={signout}>
               <h1>SIGN OUT</h1>

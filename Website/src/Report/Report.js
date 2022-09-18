83c4a572702ac8be92bc863a5cc690bd3e1db89b
pooja-seshadri <101956501+pooja-seshadri@users.noreply.github.com>
2022-09-18 20:06:15 +1000

    Call api for algorithms on frontend and display results on UI (with Ansh & Sifan)

--- Website/src/Report/Report.js
+++ Website/src/Report/Report.js
@@ -16,66 +16,79 @@ const Report = () => {
 
   const tempUUID = locations.state?.uuid;
   const [uuid, setUuid] = useState(tempUUID);
-  const resultDataPath = "http://localhost:5005/file/get/";
+  // const resultDataPath = "http://localhost:5005/file/get/";
+  const resultDataPath = "http://localhost:5005/result/get/";
+  const gifdroidResultPath = "http://localhost:5005/result/get/<uuid>/gifdroid";
+  const [owleyeImage, setOwleyeImage] = useState("");
+  const [xbotImage, setXbotImage] = useState("");
+  const [tapshoeImage, setTapshoeImage] = useState("");
 
 
-  const [reportData, updateReportData] = useState(["a2dp.Vol.AppChooser", "a2dp.Vol.CustomIntentMaker", "a2dp.Vol.EditDevice", "a2dp.Vol.main", "a2dp.Vol.ManageData", "a2dp.Vol.PackagesChooser", "a2dp.Vol.Preferences", "a2dp.Vol.ProviderList"]);
+  const [reportData, updateReportData] = useState([]);
+  const [selectedScreen, setSelectedScreen] = useState({});
 
-  // const getReportData = async (uuid) => {
-  //   const path = resultDataPath + uuid;
-  //   console.log(path);
-  //   const res = await fetch(path, {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     }
-  //   });
-  //   const data = await res.json();
-  //   console.log(data);
-  //   updateReportData(data);
-  // };
-
-  // useEffect(() => {
-  //   getReportData(uuid);
-  // }, []);
-
-
-  const [carouselItems, updateCarouselItems] = useState("../Results/content/xbot/a2dp.Vol.CustomIntentMaker.png");
-
-  const carouselTemplate = (screenshot, description) => {
-    return 0;
+  const getReportData = async (uuid) => {
+    const path = resultDataPath + "28f666de-9a79-4a03-ac74-da77acf5924a" + "/activities";
+    console.log(path);
+    const res = await fetch(path, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
+    const data = await res.json();
+    console.log("from getREportData")
+    console.log(data);
+    updateReportData(data);
   };
+
+  const getImageRequest = async (imageUrl) => {
+    const res = await fetch(imageUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
+    console.log('getImageRequest');
+    console.log(res)
+  }
+
+  useEffect(() => {
+    getReportData(uuid);
+    
+    // reportData.forEach((screen) => {
+    //   getImageRequest(screen["image"]);
+    // })
+
+  }, []);
+
+
+  // const [carouselItems, updateCarouselItems] = useState("../Results/content/xbot/a2dp.Vol.CustomIntentMaker.png");
+
+  // const carouselTemplate = (screenshot, description) => {
+  //   return 0;
+  // };
 
 
   function updateImage(screenId) {
-    const owleye = ["a2dp.Vol.AppChooser", "a2dp.Vol.CustomIntentMaker", "a2dp.Vol.EditDevice", "a2dp.Vol.main", "a2dp.Vol.ManageData", "a2dp.Vol.PackagesChooser", "a2dp.Vol.Preferences", "a2dp.Vol.ProviderList"];
-    const xbot = ["a2dp.Vol.AppChooser", "a2dp.Vol.CustomIntentMaker", "a2dp.Vol.main", "a2dp.Vol.PackagesChooser", "a2dp.Vol.ProviderList"];
+    // const owleye = ["a2dp.Vol.AppChooser", "a2dp.Vol.CustomIntentMaker", "a2dp.Vol.EditDevice", "a2dp.Vol.main", "a2dp.Vol.ManageData", "a2dp.Vol.PackagesChooser", "a2dp.Vol.Preferences", "a2dp.Vol.ProviderList"];
+    // const xbot = ["a2dp.Vol.AppChooser", "a2dp.Vol.CustomIntentMaker", "a2dp.Vol.main", "a2dp.Vol.PackagesChooser", "a2dp.Vol.ProviderList"];
 
-    console.log(screenId);
+    // if (xbot.includes(screenId)) {
+    //   document.getElementById('xbot-img').src = require("./outputs/xbot_output/outputs/a2dp.Vol_133/issues/" + screenId + "/" + screenId + ".png");
+    //   var text = readFile(screenId);
+    //   document.getElementById('xbot-text').innerHTML = text;
+    // }
+    // else {
+    //   document.getElementById('xbot-img').src = require("./outputs/storydistiller_output/outputs/a2dp.Vol_133/screenshots/" + screenId + ".png");
+    //   document.getElementById('xbot-text').innerHTML = "xBot did not find any issues with this screenshot";
+    // }
+    setSelectedScreen(screenId);
 
-    document.getElementById('base-img').src = require("./outputs/storydistiller_output/outputs/a2dp.Vol_133/screenshots/" + screenId + ".png");
-    document.getElementById('base-text').innerHTML = "This is the base screenshot that the algorithms analysed as a point of reference. Click on another image above to display the results for that screen";
-
-
-    if (owleye.includes(screenId)) {
-      document.getElementById('owleye-img').src = require("./outputs/owleye_output/output_pic/" + screenId + ".jpg");
-      document.getElementById('owleye-text').innerHTML = "owleye's heatmaps show areas where there is likely to be a bug in the GUI, where blue is generally ok and red indicates a high likelihood of an UI Display Issue.";
-    }
-    else {
-      document.getElementById('owleye-img').src = require("./outputs/storydistiller_output/outputs/a2dp.Vol_133/screenshots/" + screenId + ".png");
-      document.getElementById('owleye-text').innerHTML = "owleye did not find any issues with this screenshot";
-
-    }
-
-    if (xbot.includes(screenId)) {
-      document.getElementById('xbot-img').src = require("./outputs/xbot_output/outputs/a2dp.Vol_133/issues/" + screenId + "/" + screenId + ".png");
-      var text = readFile(screenId);
-      document.getElementById('xbot-text').innerHTML = text;
-    }
-    else {
-      document.getElementById('xbot-img').src = require("./outputs/storydistiller_output/outputs/a2dp.Vol_133/screenshots/" + screenId + ".png");
-      document.getElementById('xbot-text').innerHTML = "xBot did not find any issues with this screenshot";
-    }
+    console.log("from update image");
+    screenId["owleye"]["image"] != "" ? setOwleyeImage(screenId["owleye"]["image"]) : setOwleyeImage("")
+    screenId["xbot"]["image"] != "" ? setXbotImage(screenId["xbot"]["image"]) : setXbotImage("")
+    screenId["tapshoe"]["image"] != "" ? setTapshoeImage(screenId["tapshoe"]["image"]) : setTapshoeImage("")
   }
 
   return (
@@ -89,7 +102,7 @@ const Report = () => {
               return (
                 <img
                   className="report_img"
-                  src={require("./outputs/storydistiller_output/outputs/a2dp.Vol_133/screenshots/" + screenId + ".png")}
+                  src={screenId["image"]}
                   alt={""}
                   onClick={() => { updateImage(screenId); }}
                   style={{ "width": "200px", "height": "300px" }}
@@ -100,7 +113,53 @@ const Report = () => {
         </div>
         <div className="carousel">
           <Carousel slide={false} interval={null} variant="dark" className="horizontal-scroll-card">
-            <Carousel.Item>
+            {owleyeImage != "" && <Carousel.Item>
+                <div className="carousel-content">
+                  <img
+                    id="base-img"
+                    className="report_img"
+                    src={owleyeImage}
+                  />
+                </div>
+              </Carousel.Item>}
+              {xbotImage != "" && <Carousel.Item>
+                <div className="carousel-content">
+                  <img
+                    id="base-img"
+                    className="report_img"
+                    src={xbotImage}
+                  />
+                </div>
+              </Carousel.Item>}
+              {tapshoeImage != "" && <Carousel.Item>
+                <div className="carousel-content">
+                  <img
+                    id="base-img"
+                    className="report_img"
+                    src={tapshoeImage}
+                  />
+                </div>
+              </Carousel.Item>}
+              {/* <Carousel.Item>
+                <div className="carousel-content">
+                  <img
+                    id="base-img"
+                    className="report_img"
+                    src={t}
+                  />
+                </div>
+              </Carousel.Item>
+              <Carousel.Item>
+                <div className="carousel-content">
+                  <img
+                    id="base-img"
+                    className="report_img"
+                    src={owleyeImage}
+                  />
+                </div>
+              </Carousel.Item> */}
+
+            {/* <Carousel.Item>
               <div className="carousel-content">
                 <img
                   id="base-img"
@@ -108,19 +167,6 @@ const Report = () => {
                   alt={""}
                 />
                 <p id="base-text" className="text carousel-text">
-                  Click on an image from the section above to show the results for that screen.
-                </p>
-              </div>
-            </Carousel.Item>
-
-            <Carousel.Item>
-              <div className="carousel-content">
-                <img
-                  id="owleye-img"
-                  className="report_img"
-                  alt={""}
-                />
-                <p id="owleye-text" className="text carousel-text">
                   Click on an image from the section above to show the results for that screen.
                 </p>
               </div>
@@ -137,7 +183,7 @@ const Report = () => {
                   Click on an image from the section above to show the results for that screen.
                 </p>
               </div>
-            </Carousel.Item>
+            </Carousel.Item> */}
           </Carousel>
         </div>
       <GifdroidResult uuid={uuid}/>

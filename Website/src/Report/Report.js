ac4ad848c419557992082354b148c400a997c469
pooja-seshadri <101956501+pooja-seshadri@users.noreply.github.com>
2022-09-20 18:50:08 +1000

    Update reports page with information section with Ansh and Sifan

--- Website/src/Report/Report.js
+++ Website/src/Report/Report.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { Container} from "react-bootstrap";
+import { Container, TabContainer } from "react-bootstrap";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 
@@ -20,9 +20,10 @@ const Report = () => {
   const [xbotImage, setXbotImage] = useState("");
   const [tapshoeImage, setTapshoeImage] = useState("");
   const [tapshoeHeatmap, setTapshoeHeatmap] = useState("");
+  const [selectedScreen, setSelectedScreen] = useState({});
+  const [showIssues, setShowIssues] = useState(false);
 
   const [reportData, updateReportData] = useState([]);
-  const [selectedScreen, setSelectedScreen] = useState({});
 
   const getReportData = async (uuid) => {
     const path =
@@ -55,6 +56,8 @@ const Report = () => {
       setTapshoeImage(screenId["tapshoe"]["image"]);
     if (screenId["tapshoe"]["heatmap"] != "")
       setTapshoeHeatmap(screenId["tapshoe"]["heatmap"]);
+    
+    setShowIssues(true);
   }
 
   return (
@@ -96,42 +99,89 @@ const Report = () => {
           </Tabs>
         </div>
         <div className="carousel">
-          <Tabs
-            defaultActiveKey="profile"
-            id="uncontrolled-tab-example"
-            className="tabs-class"
-          >
-            {owleyeImage != "" && (
-              <Tab tabClassName="tab-class" eventKey="home" title="Display">
-                <div className="tab-div">
-                  <img className="issue_img" src={owleyeImage} />
-                </div>
-              </Tab>
-            )}
-            {xbotImage != "" && (
-              <Tab
-                tabClassName="tab-class"
-                eventKey="profile"
-                title="Accessibility"
-              >
-                <div className="tab-div">
-                  <img className="issue_img" src={xbotImage} />
-                </div>
-              </Tab>
-            )}
-            {tapshoeImage != "" && (
-              <Tab
-                tabClassName="tab-class"
-                eventKey="contact"
-                title="Tappability"
-              >
-                <div className="tab-div">
-                  <img className="issue_img" src={tapshoeImage} />
-                  <img className="issue_img" src={tapshoeHeatmap} />
-                </div>
-              </Tab>
-            )}
-          </Tabs>
+          {!showIssues && <div className="horizontal-images-card">
+            Please click on the above images to see the issues for each screen.
+          </div>
+          }
+          {showIssues && (
+            <Tabs
+              defaultActiveKey="profile"
+              id="uncontrolled-tab-example"
+              className="tabs-class"
+            >
+              {owleyeImage != "" && (
+                <Tab tabClassName="tab-class" eventKey="home" title="Display">
+                  <div className="tab-div">
+                    <img className="issue_img" src={owleyeImage} />
+                  </div>
+                </Tab>
+              )}
+              {xbotImage != "" && (
+                <Tab
+                  tabClassName="tab-class"
+                  eventKey="profile"
+                  title="Accessibility"
+                >
+                  <div className="tab-div">
+                    <img className="issue_img" src={xbotImage} />
+                  </div>
+                  <div id="list-example" class="list-group">
+                    <a
+                      class="list-group-item list-group-item-action"
+                      href="#list-item-1"
+                    >
+                      Item 1
+                    </a>
+                    <a
+                      class="list-group-item list-group-item-action"
+                      href="#list-item-2"
+                    >
+                      Item2
+                    </a>
+                    <a
+                      class="list-group-item list-group-item-action"
+                      href="#list-item-3"
+                    >
+                      Item 3
+                    </a>
+                    <a
+                      class="list-group-item list-group-item-action"
+                      href="#list-item-4"
+                    >
+                      Item 4
+                    </a>
+                  </div>
+                  <div
+                    data-spy="scroll"
+                    data-target="#list-example"
+                    data-offset="0"
+                    class="scrollspy-example"
+                  >
+                    <h4 id="list-item-1">Item 1</h4>
+                    <p>...</p>
+                    <h4 id="list-item-2">Item 2</h4>
+                    <p>...</p>
+                    <h4 id="list-item-3">Item 3</h4>
+                    <p>...</p>
+                    <h4 id="list-item-4">Item 4</h4>
+                    <p>...</p>
+                  </div>
+                </Tab>
+              )}
+              {tapshoeImage != "" && (
+                <Tab
+                  tabClassName="tab-class"
+                  eventKey="contact"
+                  title="Tappability"
+                >
+                  <div className="tab-div">
+                    <img className="issue_img" src={tapshoeImage} />
+                    <img className="issue_img" src={tapshoeHeatmap} />
+                  </div>
+                </Tab>
+              )}
+            </Tabs>
+          )}
         </div>
       </div>
       {/* <DroidbotMap/> */}

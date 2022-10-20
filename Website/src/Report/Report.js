a6848ccaa3f2b802e1727dd82c573ad3c0190e34
lliu240 <lliu240@student.monash.edu>
2022-10-21 10:19:05 +1100

    Temporariliy hide non-adjusted frontend components

--- Website/src/Report/Report.js
+++ Website/src/Report/Report.js
@@ -56,28 +56,12 @@ const Report = () => {
          */
         const link = resultDataPath + uuid + '/utg';
         console.log(link);
-        console.log(link);
-        console.log(link);
-        console.log(link);
-        console.log(link);
-        console.log(link);
-        console.log(link);
-        console.log(link);
-        console.log(link);
 
         await fetch(droidbotResultPath, {
             method: "GET",
             headers: {"Content-Type": "application/json"},
         }).then((response) => {
             response.json().then((json) => {
-                console.log(json);
-                console.log(json);
-                console.log(json);
-                console.log(json);
-                console.log(json);
-                console.log(json);
-                console.log(json);
-                console.log(json);
                 console.log(json);
                 // updateReportData(json);
             });
@@ -107,125 +91,6 @@ const Report = () => {
     return (
         <Container className="container-nav">
           <div className="root">
-            <p className="text-header text-centre">REPORT</p>
-            <div className="horizontal-scroll-card">
-              <Tabs
-                defaultActiveKey="screen-overview"
-                id="uncontrolled-tab-example"
-                className="tabs-class"
-              >
-                <Tab
-                  tabClassName="tab-class"
-                  eventKey="screen-overview"
-                  title="Screen Overview"
-                  id="screen-overview"
-                  active
-                  disabled
-                >
-                  <div className="horizontal-scroll-internal">
-                    {reportData.map((screenId) => {
-                        return (
-                            <img
-                              className="base_img"
-                              src={screenId["base-image"]}
-                              alt={""}
-                              onClick={() => {
-                                  updateImage(screenId);
-                              }}
-                              style={{ width: "200px", height: "300px" }}
-                            />
-                        );
-                    })}
-                  </div>
-                </Tab>
-              </Tabs>
-            </div>
-            <div className="carousel">
-              {!showIssues && (
-                  <div className="horizontal-images-card">
-                    Please click on the above images to see the issues for each
-                    screen.
-                  </div>
-              )}
-              {showIssues && (
-                  <Tabs
-                    defaultActiveKey="profile"
-                    id="uncontrolled-tab-example"
-                    className="tabs-class"
-                  >
-                    {owleyeImage != "" && (
-                        <Tab tabClassName="tab-class" eventKey="home" title="Display">
-                          <div className="tab-div">
-                            <img className="issue_img" src={owleyeImage} />
-                          </div>
-                        </Tab>
-                    )}
-                    {xbotImage != "" && (
-                        <Tab
-                          tabClassName="tab-class"
-                          eventKey="profile"
-                          title="Accessibility"
-                        >
-                          <div className="xbot-tab-div">
-                            <img className="issue_img" src={xbotImage} />
-                          </div>
-                          <div className="xbot-issues">
-                            <Tab.Container
-                              id="list-group-tabs-example"
-                              defaultActiveKey="#0"
-                            >
-                              <Row>
-                                <Col sm={4}>
-                                  {selectedScreen["xbot"]["description"].map(
-                                      (issue, index) => {
-                                          return (
-                                              <ListGroup>
-                                                <ListGroup.Item action href={`#${index}`}>
-                                                  {issue["component_type"]}
-                                                </ListGroup.Item>
-                                              </ListGroup>
-                                          );
-                                      }
-                                  )}
-                                </Col>
-                                <Col sm={8}>
-                                  {selectedScreen["xbot"]["description"].map(
-                                      (issue, index) => {
-                                          return (
-                                              <Tab.Content>
-                                                <Tab.Pane eventKey={`#${index}`}>
-                                                  {issue["issue_type"]}<br></br>
-                                                  {issue["issue_desc"]}
-                                                </Tab.Pane>
-                                              </Tab.Content>
-                                          );
-                                      }
-                                  )}
-                                </Col>
-                              </Row>
-                            </Tab.Container>
-                          </div>
-
-                          <div style={{ clear: "left" }}></div>
-                        </Tab>
-                    )}
-                    {tappableImage != "" && (
-                        <Tab
-                          tabClassName="tab-class"
-                          eventKey="contact"
-                          title="Tappability"
-                        >
-                          <div className="tab-div">
-                            <img className="issue_img" src={tappableImage} />
-                            <img className="issue_img" src={tappableHeatmap} />
-                          </div>
-                        </Tab>
-                    )}
-                  </Tabs>
-              )}
-            </div>
-            <br />
-            <br />
             <DroidbotResult uuid={uuid}/>
           </div>
           {/* <DroidbotMap/> */}
